0ec2d7b2267b2e240206e6594d0f0b3dd638c63c
Bob <bobpitaia@gmail.com>
2025-03-04 23:24:16 +0000

    Creating the queues and the exchange

---
+++ rabbitmqtest/src/main/java/com/test/rabbitmqtest/constantes/RabbitMQConstantes.java
@@ -0,0 +1,9 @@
+package com.test.rabbitmqtest.constantes;
+
+public class RabbitMQConstantes {
+
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+
+    public static final String FILA_PRECO = "PRECO";
+    
+}
