82af8c0eb705a005f9d637f404d3375d83dd20cd
Bob <bobpitaia@gmail.com>
2025-03-09 22:56:31 +0000

    Creation of consummer for PRECO with node

--- rabbitmqtest/src/main/java/com/test/rabbitmqtest/Service/RabbitMQService.java
+++ rabbitmqtest/src/main/java/com/test/rabbitmqtest/Service/RabbitMQService.java
@@ -3,6 +3,7 @@ package com.test.rabbitmqtest.Service;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class RabbitMQService {
@@ -10,8 +11,17 @@ public class RabbitMQService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public void enviaMensagem(String nomeFila, Object mensagem){
-this.rabbitTemplate.convertAndSend(nomeFila, mensagem);
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public void enviaMensagem(String nomeFila, Object mensagem) {
+        try {
+            String mensagemJson = this.objectMapper.writeValueAsString(mensagem);
+            this.rabbitTemplate.convertAndSend(nomeFila, mensagemJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    
+
 }
